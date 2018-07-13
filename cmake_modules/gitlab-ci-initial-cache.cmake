f675b6cde32efe69197b218423a183d17c6970e3
Mathieu Faverge <mathieu.faverge@inria.fr>
2018-07-13 21:36:17 +0200

    Hotfix/renaming

--- cmake_modules/gitlab-ci-initial-cache.cmake
+++ cmake_modules/gitlab-ci-initial-cache.cmake
@@ -2,5 +2,5 @@ set(BUILD_SHARED_LIBS "ON" CACHE BOOL "")
 set(CMAKE_INSTALL_PREFIX "$ENV{PWD}/install" CACHE PATH "")
 set(CMAKE_VERBOSE_MAKEFILE "ON" CACHE BOOL "")
 
-option(CHAMELEON_ENABLE_WARNING       "Enable warning messages" ON)
-option(CHAMELEON_ENABLE_COVERAGE      "Enable flags for coverage test" ON)
+option(MORSE_ENABLE_WARNING       "Enable warning messages" ON)
+option(MORSE_ENABLE_COVERAGE      "Enable flags for coverage test" ON)
