9611bdcba959d6d0f55f141b5574d78661d03a63
Mathieu Faverge <mathieu.faverge@inria.fr>
2021-05-12 15:53:41 +0200

    ci: Add -Werror to force people to remove warnings before pushing

--- cmake_modules/gitlab-ci-initial-cache.cmake
+++ cmake_modules/gitlab-ci-initial-cache.cmake
@@ -2,6 +2,8 @@ set(BUILD_SHARED_LIBS "ON" CACHE BOOL "")
 set(CMAKE_INSTALL_PREFIX "$ENV{PWD}/install" CACHE PATH "")
 set(CMAKE_VERBOSE_MAKEFILE "ON" CACHE BOOL "")
 
+set(CMAKE_C_FLAGS "-Werror")
+
 option(MORSE_ENABLE_WARNING  "Enable warning messages"        ON)
 option(MORSE_ENABLE_COVERAGE "Enable flags for coverage test" ON)
 
