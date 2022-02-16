dbf503b09ea7c0d545561fccf31ca7e9e865e0d3
Alycia Lisito <alycia.lisito@inria.fr>
2022-02-16 10:47:18 +0100

    changed wether into whether

--- runtime/starpu/interface/cham_tile_interface.c
+++ runtime/starpu/interface/cham_tile_interface.c
@@ -115,7 +115,7 @@ cti_init( void *data_interface )
 
 static void
 cti_register_data_handle( starpu_data_handle_t  handle,
-                          int                   home_node,
+                          unsigned int                   home_node,
                           void                 *data_interface )
 {
     starpu_cham_tile_interface_t *cham_tile_interface = (starpu_cham_tile_interface_t *) data_interface;
@@ -292,11 +292,11 @@ cti_compare( void *data_interface_a, void *data_interface_b )
         && ( cham_tile_a->flttype == cham_tile_b->flttype );
 }
 
-static int
-cti_alloc_compare(void *data_interface_a, void *data_interface_b)
-{
-    starpu_cham_tile_interface_t *cham_tile_a = (starpu_cham_tile_interface_t *) data_interface_a;
-    starpu_cham_tile_interface_t *cham_tile_b = (starpu_cham_tile_interface_t *) data_interface_b;
+/* static int */
+/* cti_alloc_compare(void *data_interface_a, void *data_interface_b) */
+/* { */
+/*     starpu_cham_tile_interface_t *cham_tile_a = (starpu_cham_tile_interface_t *) data_interface_a; */
+/*     starpu_cham_tile_interface_t *cham_tile_b = (starpu_cham_tile_interface_t *) data_interface_b; */
 
     /* Two matrices are considered compatible if they have the same allocated size */
     return ( cham_tile_a->allocsize == cham_tile_b->allocsize );
