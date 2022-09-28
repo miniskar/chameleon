035ab4aab97af286944dc5b47715d57596145c45
Mathieu Faverge <mathieu.faverge@inria.fr>
2022-09-28 21:55:31 +0200

    coreblas: add missing files to handle mixed precision conversion functions

---
+++ coreblas/include/coreblas/coreblas_zctile.h
@@ -0,0 +1,23 @@
+/**
+ *
+ * @file coreblas_zctile.h
+ *
+ * @copyright 2019-2022 Bordeaux INP, CNRS (LaBRI UMR 5800 ), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ * @brief Chameleon CPU kernel CHAM_tile_t interface
+ *
+ * @version 1.2.0
+ * @author Mathieu Faverge
+ * @author Florent Pruvost
+ * @date 2022-02-22
+ * @precisions mixed zc -> ds
+ *
+ */
+#ifndef _coreblas_zctile_h_
+#define _coreblas_zctile_h_
+
+void TCORE_clag2z( int M, int N, const CHAM_tile_t *A, CHAM_tile_t *B );
+void TCORE_zlag2c( int M, int N, const CHAM_tile_t *A, CHAM_tile_t *B, int *info );
+
+#endif /* _coreblas_zctile_h_ */
