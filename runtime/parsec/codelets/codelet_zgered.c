e256459a497269cb3e26053459c6fc0b110521c2
Mathieu Faverge <mathieu.faverge@inria.fr>
2023-07-11 15:18:20 +0200

    parsec/codelets: add missing gerst/gered kernels

---
+++ runtime/parsec/codelets/codelet_zgered.c
@@ -0,0 +1,34 @@
+/**
+ *
+ * @file parsec/codelet_zgered.c
+ *
+ * @copyright 2023-2023 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ ***
+ *
+ * @brief Chameleon zgered PaRSEC codelet
+ *
+ * @version 1.3.0
+ * @author Mathieu Faverge
+ * @date 2023-07-06
+ * @precisions normal z -> d
+ *
+ */
+#include "chameleon_parsec.h"
+
+void INSERT_TASK_zgered( const RUNTIME_option_t *options,
+                         double threshold, double Anorm, int m, int n,
+                         const CHAM_desc_t *A, int Am, int An )
+{
+    fprintf( stderr, "WARNING: gered kernel is not available with PaRSEC\n" );
+
+    (void)options;
+    (void)threshold;
+    (void)Anorm;
+    (void)m;
+    (void)n;
+    (void)A;
+    (void)Am;
+    (void)An;
+}
---
+++ runtime/parsec/codelets/codelet_zgerst.c
@@ -0,0 +1,32 @@
+/**
+ *
+ * @file parsec/codelet_zgerst.c
+ *
+ * @copyright 2023-2023 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                      Univ. Bordeaux. All rights reserved.
+ *
+ ***
+ *
+ * @brief Chameleon zgerst PaRSEC codelet
+ *
+ * @version 1.3.0
+ * @author Mathieu Faverge
+ * @date 2023-07-06
+ * @precisions normal z -> d
+ *
+ */
+#include "chameleon_parsec.h"
+
+void INSERT_TASK_zgerst( const RUNTIME_option_t *options,
+                         int m, int n,
+                         const CHAM_desc_t *A, int Am, int An )
+{
+    fprintf( stderr, "WARNING: gerst kernel is not available with PaRSEC\n" );
+
+    (void)options;
+    (void)m;
+    (void)n;
+    (void)A;
+    (void)Am;
+    (void)An;
+}
