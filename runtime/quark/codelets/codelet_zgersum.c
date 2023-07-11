6eaa09a3d80d7c1fb5a47c9c27bfba15b280ea87
Mathieu Faverge <mathieu.faverge@inria.fr>
2023-07-11 15:18:10 +0200

    quark/codelets: add missing gerst/gered kernels

--- runtime/quark/codelets/codelet_zgersum.c
+++ runtime/quark/codelets/codelet_zgersum.c
@@ -1,6 +1,6 @@
 /**
  *
- * @file starpu/codelet_zgersum.c
+ * @file quark/codelet_zgersum.c
  *
  * @copyright 2009-2014 The University of Tennessee and The University of
  *                      Tennessee Research Foundation. All rights reserved.
